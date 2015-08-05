ded39cf3891362cbd8dfe12bad196aa2a9f9638c
751890720 <751890720@qq.com>
2015-08-05 01:58:08 +0000

    第一个文件测试

---
+++ TestObject.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  MyCarthageTest
+//
+//  Created by zhongwu on 15/8/5.
+//  Copyright (c) 2015年 zhongwu. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
