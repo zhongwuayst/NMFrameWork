1a1939f64e8f9e8b6da8f297e5cf17805aa9d997
751890720 <751890720@qq.com>
2015-08-06 23:56:54 +0000

    类库

---
+++ ZWTools.framework/Headers/NetworkClass.h
@@ -0,0 +1,13 @@
+//
+//  NetworkClass.h
+//  ZWTools
+//
+//  Created by zhongwu on 15/7/30.
+//  Copyright (c) 2015年 zhongwu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkClass : NSObject
+-(void) test;
+@end
---
+++ ZWTools.framework/Headers/ZWTools.h
@@ -0,0 +1,21 @@
+//
+//  ZWTools.h
+//  ZWTools
+//
+//  Created by zhongwu on 15/7/30.
+//  Copyright (c) 2015年 zhongwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NetworkClass.h"
+
+//! Project version number for ZWTools.
+FOUNDATION_EXPORT double ZWToolsVersionNumber;
+
+//! Project version string for ZWTools.
+FOUNDATION_EXPORT const unsigned char ZWToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZWTools/PublicHeader.h>
+
+
